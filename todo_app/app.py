2089c949142e4914f699a59ccceac8c025fb9af7
Emilie <emilie.stone@softwire.com>
2022-07-12 16:42:45 +0100

    using built in session fraemwork

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,22 +1,16 @@
 from flask import Flask, render_template, request, redirect, url_for
-
+from todo_app.data.session_items import get_items, add_item
 from todo_app.flask_config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config())
 
-def update_csv(task_name):
-    with open("./todo_app/todo_list.csv", 'a') as file:
-        file.write(f"\n{task_name}")
-
 @app.route('/')
 def index():
-    with open("./todo_app/todo_list.csv") as file:
-        todo_items = [  {'task': line } for line in file ]
-    return render_template('index.html', items=todo_items)
+    return render_template('index.html', items=get_items())
 
 @app.route('/submit', methods = ["POST"])
 def submit_review():
     task_name = request.form.get("task-name")
-    update_csv(task_name)
+    add_item(task_name)
     return redirect(url_for('index'))
