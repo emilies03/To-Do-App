71ad2019b75eea3632b31d6862b119994a120cc9
Emilie <emilie.stone@softwire.com>
2022-07-14 18:19:00 +0100

    tick box task completion

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
-from todo_app.data.session_items import get_items, add_item
+from todo_app.data.session_items import get_items, add_item, update_task_status
 from todo_app.flask_config import Config
 
 app = Flask(__name__)
@@ -13,4 +13,10 @@ def index():
 def submit_review():
     task_name = request.form.get("task-name")
     add_item(task_name)
+    return redirect(url_for('index'))
+
+@app.route('/update', methods = ["POST"])
+def update_item_status():
+    item_id = request.form.get("item-id")
+    update_task_status(item_id)
     return redirect(url_for('index'))
