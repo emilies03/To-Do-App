9d1ded303f9a38e68f62a6333979f331aedfdf88
Emilie <emilie.stone@softwire.com>
2022-08-23 12:27:16 +0100

    adding new task working

--- todo_app/data/api_client.py
+++ todo_app/data/api_client.py
@@ -5,3 +5,6 @@ def get_cards_request():
     result = requests.get(f'https://api.trello.com/1/boards/{os.getenv("BOARD_ID")}/lists?cards=open&key={os.getenv("SECRET_KEY")}&token={os.getenv("TOKEN")}')
     if (result.status_code == 200):
         return result.json()
+
+def create_new_card(card_name):
+    requests.post(f'https://api.trello.com/1/cards?name={card_name}&idList={os.getenv("TO_DO_LIST_ID")}&key={os.getenv("SECRET_KEY")}&token={os.getenv("TOKEN")}')
