19e80107b22a7a9725cc3e11c000742423ed9a5d
Emilie <emilie.stone@softwire.com>
2022-09-06 18:00:21 +0100

    unit tests

--- todo_app/view_models/cards_view_model.py
+++ todo_app/view_models/cards_view_model.py
@@ -5,3 +5,15 @@ class CardsViewModel:
     @property
     def cards(self):
         return self._cards
+
+    @property
+    def todo_items(self):
+        return [c for c in self.cards if c.status == "To Do"]
+
+    @property
+    def started_items(self):
+        return [c for c in self.cards if c.status == "Started"]
+
+    @property
+    def done_items(self):
+        return [c for c in self.cards if c.status == "Done"]    
