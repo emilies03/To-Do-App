b68c16bc72ba849dc2accca5a357272eec5ed972
Emilie <emilie.stone@softwire.com>
2023-05-17 15:56:33 +0100

    adding tasks without description working

--- todo_app/data/api_client.py
+++
@@ -1,5 +0,0 @@
-import os
-import requests
-
-def create_new_card(card_name):
-    requests.post(f'https://api.trello.com/1/cards?name={card_name}&idList={os.getenv("TO_DO_LIST_ID")}&key={os.getenv("API_KEY")}&token={os.getenv("TOKEN")}')
--- todo_app/data/session_items.py
+++ todo_app/data/session_items.py
@@ -1,6 +1,5 @@
-from todo_app.data.api_client import create_new_card
 from todo_app.data.item import Item
-from todo_app.data.database_client import get_tasks_from_db, update_task_status_in_db
+from todo_app.data.database_client import get_tasks_from_db, update_task_status_in_db, add_task_to_db
 
 def get_items():
     database_results = get_tasks_from_db()
@@ -17,7 +16,7 @@ def update_task_status(card_id, card_status):
     update_task_status_in_db(card_id, updated_status)
 
 def add_item(item_name):
-    create_new_card(item_name)
+    add_task_to_db(item_name)
 
 def get_updated_status_string(card_status):
     if (card_status == "To Do"):
