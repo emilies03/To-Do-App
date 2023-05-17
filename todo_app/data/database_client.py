c065d8e9684a88d0d918e7d7f9461194a07d5ade
Emilie <emilie.stone@softwire.com>
2023-05-17 15:56:33 +0100

    adding tasks without description working

--- todo_app/data/database_client.py
+++ todo_app/data/database_client.py
@@ -1,16 +1,25 @@
 import os
-import requests
 import pymongo
-import pprint
 
 def get_tasks_from_db():
-    client = pymongo.MongoClient(os.getenv("PRIMARY_DB_CONNECTION_STRING"))
-    tasksCollection = client[os.getenv("DATABASE_NAME")]['tasks']
+    tasksCollection = get_tasks_collection_from_db()
     return tasksCollection.find()
 
 def update_task_status_in_db(task_id, new_status):
-    client = pymongo.MongoClient(os.getenv("PRIMARY_DB_CONNECTION_STRING"))
-    tasksCollection = client[os.getenv("DATABASE_NAME")]['tasks']
+    tasksCollection = get_tasks_collection_from_db()
     findTaskQuery = {"_id" : task_id}
     newStatus = {"$set": {"status" : new_status}}
     tasksCollection.update_one(findTaskQuery, newStatus)
+
+def add_task_to_db(task_name):
+    tasksCollection = get_tasks_collection_from_db()
+    newTask = { "name": task_name,
+               "description": "",
+               "status": "To Do" 
+    }
+    tasksCollection.insert_one(newTask)
+
+
+def get_tasks_collection_from_db():
+    client = pymongo.MongoClient(os.getenv("PRIMARY_DB_CONNECTION_STRING"))
+    return client[os.getenv("DATABASE_NAME")]['tasks']
