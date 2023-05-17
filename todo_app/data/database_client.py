26bdd741b549057a182e794de3fd0a338d625a58
Emilie <emilie.stone@softwire.com>
2023-05-17 15:43:07 +0100

    Updating status in db

--- todo_app/data/database_client.py
+++ todo_app/data/database_client.py
@@ -7,3 +7,10 @@ def get_tasks_from_db():
     client = pymongo.MongoClient(os.getenv("PRIMARY_DB_CONNECTION_STRING"))
     tasksCollection = client[os.getenv("DATABASE_NAME")]['tasks']
     return tasksCollection.find()
+
+def update_task_status_in_db(task_id, new_status):
+    client = pymongo.MongoClient(os.getenv("PRIMARY_DB_CONNECTION_STRING"))
+    tasksCollection = client[os.getenv("DATABASE_NAME")]['tasks']
+    findTaskQuery = {"_id" : task_id}
+    newStatus = {"$set": {"status" : new_status}}
+    tasksCollection.update_one(findTaskQuery, newStatus)
--- todo_app/data/session_items.py
+++ todo_app/data/session_items.py
@@ -1,7 +1,6 @@
-import os
 from todo_app.data.api_client import create_new_card, move_card_to_list
 from todo_app.data.item import Item
-from todo_app.data.database_client import get_tasks_from_db
+from todo_app.data.database_client import get_tasks_from_db, update_task_status_in_db
 
 def get_items():
     database_results = get_tasks_from_db()
@@ -14,16 +13,16 @@ def get_items():
     return tasks_array
 
 def update_task_status(card_id, card_status):
-    next_list_id = get_next_list_id(card_status)
-    move_card_to_list(card_id, next_list_id)
+    updated_status = get_updated_status_string(card_status)
+    update_task_status_in_db(card_id, updated_status)
 
 def add_item(item_name):
     create_new_card(item_name)
 
-def get_next_list_id(card_status):
+def get_updated_status_string(card_status):
     if (card_status == "To Do"):
-        return os.getenv("STARTED_LIST_ID")
+        return "Started"
     if (card_status == "Started"):
-        return os.getenv("DONE_LIST_ID")
+        return "Done"
     if (card_status == "Done"):
-        return os.getenv("TO_DO_LIST_ID")
+        return "To Do"
