1b6a79a0575b471afbe932e1393917ebaa7c53c8
Emilie <emilie.stone@softwire.com>
2023-08-31 15:42:07 +0100

    Tidy up code

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
-from todo_app.data.session_items import get_items, add_item, update_task_status, delete_item
+from todo_app.data.session_items import get_items, update_task_status
+from todo_app.data.database_client import add_task_to_db, delete_item_in_db
 from todo_app.flask_config import Config
 from todo_app.view_models.items_view_model import ItemsViewModel
 
@@ -13,17 +14,17 @@ def create_app():
         return render_template('index.html', view_model = items_view_model)
 
     @app.route('/submit', methods = ["POST"])
-    def submit_new_task():
+    def create_new_task():
         task_name = request.form.get("name")
         task_description = request.form.get("description")
-        add_item(task_name, task_description)
+        add_task_to_db(task_name, task_description)
         return redirect(url_for('index'))
 
     @app.route('/update', methods = ["POST"])
     def update_item_status():
         card_id = request.form.get("item-id")
         if (request.form['action'] == "delete"):
-            delete_item(card_id)
+            delete_item_in_db(card_id)
             return redirect(url_for('index'))
         else:
             card_status = request.form['action']
