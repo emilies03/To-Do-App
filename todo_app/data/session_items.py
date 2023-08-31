5e3e191cb181ecdc86e605bdd3869886470fe845
Emilie <emilie.stone@softwire.com>
2023-08-31 15:42:07 +0100

    Tidy up code

--- todo_app/data/session_items.py
+++ todo_app/data/session_items.py
@@ -1,5 +1,5 @@
 from todo_app.data.item import Item
-from todo_app.data.database_client import get_tasks_from_db, update_task_status_in_db, add_task_to_db, delete_item_in_db
+from todo_app.data.database_client import get_tasks_from_db, update_task_status_in_db
 
 def get_items():
     database_results = get_tasks_from_db()
@@ -15,12 +15,6 @@ def get_items():
 def update_task_status(card_id, card_status):
     updated_status = get_updated_status_string(card_status)
     update_task_status_in_db(card_id, updated_status)
-
-def add_item(item_name, item_description):
-    add_task_to_db(item_name, item_description)
-
-def delete_item(card_id):
-    delete_item_in_db(card_id)    
 
 def get_updated_status_string(card_status):
     if (card_status == "To Do"):
