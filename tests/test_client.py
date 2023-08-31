5d1bbf4105a2651dc2042ab2743faae0fb8c4e31
Emilie <emilie.stone@softwire.com>
2023-08-31 16:22:01 +0100

    Fix Tests

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,40 +1,22 @@
 import pytest
-import os
-import requests
+import mongomock
 from dotenv import load_dotenv, find_dotenv
 from todo_app import app
+from todo_app.data.database_client import add_task_to_db
 
 @pytest.fixture
 def client():
     file_path = find_dotenv('.env.test')
     load_dotenv(file_path, override=True)
-    test_app = app.create_app()
-    with test_app.test_client() as client:
-        yield client
 
-def test_index_page(monkeypatch, client):
-    monkeypatch.setattr(requests, 'get', get_lists_stub)
+    with mongomock.patch(servers=(('fakemongo.com', 27017),)):
+        test_app = app.create_app()
+        with test_app.test_client() as client:
+            yield client
+
+def test_index_page(client):
+    add_task_to_db("task_name", "task_description")
     response = client.get('/')
 
     assert response.status_code == 200
-    assert "Test card" in response.data.decode()
-
-class StubResponse():
-    def __init__(self, fake_response_data):
-        self.fake_response_data = fake_response_data
-
-    def json(self):
-        return self.fake_response_data
-
-    def raise_for_status(self):
-        return    
-
-def get_lists_stub(url):
-    fake_response_data = None
-    if url == f'https://api.trello.com/1/boards/{os.getenv("BOARD_ID")}/lists?cards=open&key={os.getenv("API_KEY")}&token={os.getenv("TOKEN")}':
-        fake_response_data = [{
-            'id': '123abc',
-            'name': 'To Do',
-            'cards': [{'id': '456', 'name': 'Test card', 'desc': 'task description'}]
-        }]
-    return StubResponse(fake_response_data)
+    assert "task_name" in response.data.decode()
