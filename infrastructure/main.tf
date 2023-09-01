0535dd4fd3baff793bd3aa335d913257445bd269
Emilie <emilie.stone@softwire.com>
2023-09-01 14:42:03 +0100

    Add database to terraform

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -36,3 +36,46 @@ resource "azurerm_linux_web_app" "main" {
     }
   }
 }
+
+resource "azurerm_cosmosdb_account" "main" {
+  name                = "em-todoapp-db"
+  location            = data.azurerm_resource_group.main.location
+  resource_group_name = data.azurerm_resource_group.main.name
+  offer_type          = "Standard"
+  kind                = "MongoDB"
+  
+  capabilities {
+    name = "EnableServerless"
+  }
+
+  capabilities {
+    name = "EnableAggregationPipeline"
+  }
+
+  capabilities {
+    name = "mongoEnableDocLevelTTL"
+  }
+
+  capabilities {
+    name = "MongoDBv3.4"
+  }
+
+  capabilities {
+    name = "EnableMongo"
+  }
+
+  consistency_policy {
+    consistency_level = "Session"
+  }
+
+  geo_location {
+    location          = "westus"
+    failover_priority = 0
+  }
+}
+
+resource "azurerm_cosmosdb_mongo_database" "main" {
+  name                = "ToDoAppDatabase"
+  resource_group_name = azurerm_cosmosdb_account.main.resource_group_name
+  account_name        = azurerm_cosmosdb_account.main.name
+}
