8437c4097f155a51780d6773e1059f9411d8d9b7
Emilie <emilie.stone@softwire.com>
2023-09-01 15:50:49 +0100

    Only make single MongoClient

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,34 +1,34 @@
 from flask import Flask, render_template, request, redirect, url_for
-from todo_app.data.session_items import get_items, update_task_status
-from todo_app.data.database_client import add_task_to_db, delete_item_in_db
+from todo_app.data.database_client import DatabaseClient
 from todo_app.flask_config import Config
 from todo_app.view_models.items_view_model import ItemsViewModel
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config())
+    client = DatabaseClient()
 
     @app.route('/')
     def index():
-        items_view_model = ItemsViewModel(get_items())
+        items_view_model = ItemsViewModel(client.get_items())
         return render_template('index.html', view_model = items_view_model)
 
     @app.route('/submit', methods = ["POST"])
     def create_new_task():
         task_name = request.form.get("name")
         task_description = request.form.get("description")
-        add_task_to_db(task_name, task_description)
+        client.add_task_to_db(task_name, task_description)
         return redirect(url_for('index'))
 
     @app.route('/update', methods = ["POST"])
     def update_item_status():
         card_id = request.form.get("item-id")
         if (request.form['action'] == "delete"):
-            delete_item_in_db(card_id)
+            client.delete_item_in_db(card_id)
             return redirect(url_for('index'))
         else:
             card_status = request.form['action']
-            update_task_status(card_id, card_status)
+            client.update_task_status(card_id, card_status)
             return redirect(url_for('index'))
 
     return app
--- todo_app/data/database_client.py
+++ todo_app/data/database_client.py
@@ -1,29 +1,50 @@
 import os
 import pymongo
+from todo_app.data.item import Item
 
-def get_tasks_from_db():
-    tasksCollection = get_tasks_collection_from_db()
-    return tasksCollection.find()
+class DatabaseClient:
+    def __init__(self):
+        self.mongo_db_client = pymongo.MongoClient(os.getenv("PRIMARY_DB_CONNECTION_STRING"))
+        self.tasks = self.mongo_db_client[os.getenv("DATABASE_NAME")]['tasks']
 
-def update_task_status_in_db(task_id, new_status):
-    tasksCollection = get_tasks_collection_from_db()
-    findTaskQuery = {"_id" : task_id}
-    newStatus = {"$set": {"status" : new_status}}
-    tasksCollection.update_one(findTaskQuery, newStatus)
+    def get_tasks_from_db(self):
+        return self.tasks.find()
 
-def add_task_to_db(task_name, task_description):
-    tasksCollection = get_tasks_collection_from_db()
-    newTask = { "name": task_name,
-               "description": task_description,
-               "status": "To Do" 
-    }
-    tasksCollection.insert_one(newTask)
+    def update_task_status_in_db(self, task_id, new_status):
+        findTaskQuery = {"_id" : task_id}
+        newStatus = {"$set": {"status" : new_status}}
+        self.tasks.update_one(findTaskQuery, newStatus)
 
-def delete_item_in_db(task_id):
-    tasksCollection = get_tasks_collection_from_db()
-    findTaskQuery = {"_id" : task_id}
-    tasksCollection.delete_one(findTaskQuery)
+    def add_task_to_db(self, task_name, task_description):
+        newTask = { "name": task_name,
+                "description": task_description,
+                "status": "To Do" 
+        }
+        self.tasks.insert_one(newTask)
 
-def get_tasks_collection_from_db():
-    client = pymongo.MongoClient(os.getenv("PRIMARY_DB_CONNECTION_STRING"))
-    return client[os.getenv("DATABASE_NAME")]['tasks']
+    def delete_item_in_db(self, task_id):
+        findTaskQuery = {"_id" : task_id}
+        self.tasks.delete_one(findTaskQuery)
+    
+    def get_items(self):
+        database_results = self.get_tasks_from_db()
+        if database_results == None:
+            return []
+        tasks_array = [
+            Item.from_database_entry(task)
+            for task in database_results
+        ]
+        tasks_array.sort(key=lambda x: x.get_status(), reverse=True)
+        return tasks_array
+    
+    def update_task_status(self, card_id, card_status):
+        updated_status = self.get_updated_status_string(card_status)
+        self.update_task_status_in_db(card_id, updated_status)
+
+    def get_updated_status_string(self, card_status):
+        if (card_status == "To Do"):
+            return "Started"
+        if (card_status == "Started"):
+            return "Done"
+        if (card_status == "Done"):
+            return "To Do"
