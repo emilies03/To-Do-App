85606d56ab2a66c43340d6889a3e3acf21ca6b5e
Emilie <emilie.stone@softwire.com>
2023-09-01 13:54:47 +0100

    add basic hello world to terraform

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -14,3 +14,25 @@ provider "azurerm" {
 data "azurerm_resource_group" "main" {
   name     = "Cohort25_EmiSto_ProjectExercise"
 }
+
+resource "azurerm_service_plan" "main" {
+  name                = "terraformed-asp"
+  location            = data.azurerm_resource_group.main.location
+  resource_group_name = data.azurerm_resource_group.main.name
+  os_type             = "Linux"
+  sku_name            = "B1"
+}
+
+resource "azurerm_linux_web_app" "main" {
+  name                = "em-todoapp"
+  location            = data.azurerm_resource_group.main.location
+  resource_group_name = data.azurerm_resource_group.main.name
+  service_plan_id     = azurerm_service_plan.main.id
+
+  site_config {
+    application_stack {
+      docker_image_name   = "appsvcsample/python-helloworld:latest"
+      docker_registry_url = "https://index.docker.io"
+    }
+  }
+}
