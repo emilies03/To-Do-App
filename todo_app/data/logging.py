6819fb446304c929a0c3dcadf3ca2afe8bbb87e0
Emilie <emilie.stone@softwire.com>
2023-09-06 10:32:50 +0100

    loggly clean up

--- todo_app/data/logging.py
+++ todo_app/data/logging.py
@@ -7,14 +7,18 @@ LOGGER_NAME = "todo_app"
 
 def setupLogger():
     streamHandler = StreamHandler()
-    logglyHandler = HTTPSHandler(f'https://logs-01.loggly.com/inputs/{os.getenv("LOGGLY_TOKEN")}/tag/todo-app')
-    logglyHandler.setFormatter(Formatter("[%(asctime)s] %(levelname)s in %(module)s: %(message)s"))
+
+    handlers = [streamHandler]
+    if os.getenv("LOGGLY_TOKEN") is not None:
+        logglyHandler = HTTPSHandler(f'https://logs-01.loggly.com/inputs/{os.getenv("LOGGLY_TOKEN")}/tag/todo-app')
+        logglyHandler.setFormatter(Formatter("[%(asctime)s] %(levelname)s in %(module)s: %(message)s"))
+        handlers.append(logglyHandler)
 
     logLevel = os.environ.get('LOG_LEVEL')
     if logLevel == None :
         logLevel = "INFO"
     
-    logging.basicConfig(level = logLevel, handlers= [streamHandler, logglyHandler])
+    logging.basicConfig(level = logLevel, handlers= handlers)
 
 def getLogger():
     return logging.getLogger(LOGGER_NAME)
